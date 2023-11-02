e6cd9599137b33eae85bb0696ccb2efceaa531f5
Akash <akashsawant9960@gmail.com>
2023-11-02 14:25:25 +0530

    sample commit

---
+++ src/test/java/com/luv2code/springboot/employeecruddemo/EmployeecruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.employeecruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
